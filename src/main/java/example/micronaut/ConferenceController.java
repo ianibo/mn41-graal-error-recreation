077d262ebd65e5c46cb8c0f026cf1f37ed4128f2
Ian Ibbotson <ian.ibbotson@k-int.com>
2023-09-09 17:41:38 +0100

    Added some dependencies - all working sofar except no log output

--- src/main/java/example/micronaut/ConferenceController.java
+++ src/main/java/example/micronaut/ConferenceController.java
@@ -2,13 +2,18 @@ package example.micronaut;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/conferences") 
 public class ConferenceController {
 
+    private static final Logger log = LoggerFactory.getLogger(ConferenceController.class);
+
     private final ConferenceService conferenceService;
 
     public ConferenceController(ConferenceService conferenceService) { 
+        log.debug("ConferenceController::construct");
         this.conferenceService = conferenceService;
     }
 
--- src/main/java/mn4graalerr/Application.java
+++ src/main/java/mn4graalerr/Application.java
@@ -2,9 +2,28 @@ package mn4graalerr;
 
 import io.micronaut.runtime.Micronaut;
 
+import java.net.URI;
+import java.net.URL;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+import io.micronaut.core.annotation.TypeHint;
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@TypeHint(value = { Instant[].class, ZonedDateTime[].class, URI[].class, URL[].class })
+@OpenAPIDefinition(info = @Info(title = "Test", description = "Test", version = "1.0.0"))
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+	System.out.println("Start1");
+        log.info("Startr2");
         Micronaut.run(Application.class, args);
     }
 }
